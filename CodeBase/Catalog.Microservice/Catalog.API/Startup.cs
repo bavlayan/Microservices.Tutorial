6034cd881524e2e870e282ae5ad6e561d2da27ad
Batuhan AVLAYAN <b.avlayan@gmail.com>
2020-11-25 23:54:12 +0300

    Added docker files

--- CodeBase/Catalog.Microservice/Catalog.API/Startup.cs
+++ CodeBase/Catalog.Microservice/Catalog.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +40,11 @@ namespace Catalog.API
             services.AddTransient<ICatalogContext, CatalogContext>();
 
             services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +62,13 @@ namespace Catalog.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
             });
         }
     }
